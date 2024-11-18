ca629a0134f818a18af8d83478848ecaa79777da
tatiana-baranova <syniovych.tanya@gmail.com>
2024-11-18 17:54:34 +0000

    Deploying to gh-pages from @ tatiana-baranova/goit-js-hw-09@118b62b94d59b848ef2248c548699b855ae40938 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,12 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Form");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const s=document.querySelector(".container");console.log(s);const m=`<form class="feedback-form" autocomplete="off">
+<label class="form-label">
+Email
+<input class="form-input" type="email" name="email" autofocus />
+</label>
+<label class="form-label">
+Message
+<textarea class="form-textarea" name="message" rows="8"></textarea>
+</label>
+<button class="form-button" type="submit">Submit</button>
+</form>`;let t={email:"",message:""};s.insertAdjacentHTML("beforeend",m);const a=document.querySelector(".feedback-form");a.querySelector(".form-textarea");const o="feedback-form-state";n();a.addEventListener("input",e=>{e.target.name&&(t[e.target.name]=e.target.value,localStorage.setItem(o,JSON.stringify(t)))});a.addEventListener("submit",e=>{if(e.preventDefault(),!t.email||!t.message){alert("Fill please all fields");return}console.log("Відправлено:",t),e.currentTarget.reset(),localStorage.removeItem(o),t={email:"",message:""}});function n(){const e=localStorage.getItem(o);e&&(t=JSON.parse(e),Object.keys(t).forEach(r=>{const l=a.querySelector(`[name="${r}"]`);l&&(l.value=t[r])}))}
 //# sourceMappingURL=2-form.js.map
